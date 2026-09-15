59ea7a3925982007399dd68ee89a4a642603f91e
akshay-sreekumar-kizhakkedath <akshaysreekumarkizhakkedath@gmail.com>
2026-09-15 23:58:37 +0530

    fix: use Vite BASE_URL for BrowserRouter basename instead of window.location

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
 
-const basename = window.location.pathname.replace(/\/+$/, '')
+const basename = import.meta.env.BASE_URL.replace(/\/+$/, '')
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
